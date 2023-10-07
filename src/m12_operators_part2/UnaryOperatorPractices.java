c0159d1d9737486b5b3505de47013aa612b2475a
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-10-07 13:51:13 +0200

    Module12 Operators Part2 - Lab

---
+++ src/m12_operators_part2/UnaryOperatorPractices.java
@@ -0,0 +1,55 @@
+package m12_operators_part2;
+
+// Guess the result of each statement.
+
+public class UnaryOperatorPractices {
+
+    public static void main(String[] args) {
+
+        // First Question
+//
+//        int x = 2;
+//        int y = x++;
+//
+//        System.out.println(y);
+
+        // Second Question
+
+//        int x = 2;
+//
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        // Third Question
+
+//        int x = 2;
+//
+//        System.out.println(--x);
+//        System.out.println(x);
+
+        // Fourth Question
+
+//        int x = 8;
+//        int y = x--;
+//
+//        System.out.println(y);
+//        System.out.println(x);
+
+        // Fifth Question
+
+//        int a = 50;
+//        a = --a + a++ + a-- + a++;
+//
+//        System.out.println(a);
+
+        // Sixth Question
+
+        int x = 4;
+        int y = --x * 4 - x++;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+
+}
